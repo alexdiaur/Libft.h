c58729e8d4f32e1a7385cbd4a714fedf260decab
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-10-29 23:57:02 +0200

    adds corrected strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 00:36:49 by fdiaz             #+#    #+#             */
-/*   Updated: 2022/09/24 12:53:41 by ale              ###   ########.fr       */
+/*   Updated: 2022/10/29 23:54:52 by ale              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	size_t	srcsize;
 	size_t	i;
+	size_t	srclen;
 
 	i = 0;
-	srcsize = ft_strlen(src);
-	if (dstsize != '\0')
+	srclen = ft_strlen(src);
+	if (!dstsize)
+		return (srclen);
+	while (src[i] != '\0' && i < (dstsize -1))
 	{
-		while (*src != '\0' && i < (dstsize - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-	dst[i] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
-	return (srcsize);
+	dst[i] = '\0';
+	return (srclen);
 }
 
 /*#include <stdio.h>
