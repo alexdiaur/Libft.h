70b1d88b440e0c57d50d38c6538bd9d5fef9f95d
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-10-29 15:39:13 +0200

    adds corrected strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/24 13:00:11 by ale               #+#    #+#             */
-/*   Updated: 2022/09/24 13:27:45 by ale              ###   ########.fr       */
+/*   Updated: 2022/10/29 15:29:14 by ale              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,28 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
+	size_t	srclen;
 
-	i = ft_strlen(dst);
+	i = 0;
 	j = 0;
-	if (dstsize <= ft_strlen(dst))
-		return (dstsize + ft_strlen(src));
-	while (src[j] != '\0' && i + 1 < dstsize)
-	{
-		dst[i] = src[j];
+	srclen = ft_strlen(src);
+	while (dst[i] && i < dstsize)
 		i++;
+	while (src[j] && i + j + 1 < dstsize)
+	{
+		dst[i + j] = src[j];
 		j++;
 	}
-	dst[i] = '\0';
-	return (ft_strlen(dst) + ft_strlen(src));
+	if (j != 0)
+		dst[i + j] = '\0';
+	return (srclen + i);
 }
 
 /*int main()
 {
-    char destine[16] = "Loving Oio to ";
+    char destine[] = "Loving Oio to ";
 	char source[] = "the moon and back";
-	size_t i = ft_strlen(destine) + ft_strlen(source);
+	size_t i = ft_strlen(source) + ft_strlen(destine) + 1;
 
 	ft_strlcat(destine, source, i);
 	printf("Con mi strlcay el string queda así: %s\n", destine);
