015f00030eacc7d30f1320bad7d4d5d5d8bd3cd6
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-12-08 07:40:58 +0100

    adds memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 06:40:15 by ale               #+#    #+#             */
+/*   Updated: 2022/12/08 07:24:46 by ale              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*s1aux;
+	unsigned char	*s2aux;
+
+	s1aux = (unsigned char *) s1;
+	s2aux = (unsigned char *) s2;
+	while (n > 0)
+	{
+		if (*s1aux != *s2aux)
+			return (*s1aux - *s2aux);
+		s1aux++;
+		s2aux++;
+		n--;
+	}
+	return (0);
+}
