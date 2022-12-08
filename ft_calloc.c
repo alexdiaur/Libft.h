bb438b075bdabe57fd6f012fd95db76deae913a7
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-12-08 07:41:34 +0100

    adds calloc

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/08 07:37:34 by ale               #+#    #+#             */
+/*   Updated: 2022/12/08 07:38:00 by ale              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*pointer;
+	size_t	aux;
+
+	aux = count * size;
+	if (aux == 1 && count != 1)
+		return (NULL);
+	pointer = malloc(aux);
+	if (!pointer)
+		return (NULL);
+	ft_bzero(pointer, count * size);
+	return (pointer);
+}
