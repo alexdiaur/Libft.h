bb1204e44869f956017a70f09653266df7ae453e
Alejandra Diaz <francys.diazurribarri@gmail.com>
2022-12-08 07:44:58 +0100

    modify libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ale <ale@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 10:34:45 by fdiaz             #+#    #+#             */
-/*   Updated: 2022/10/30 01:13:34 by ale              ###   ########.fr       */
+/*   Updated: 2022/12/08 06:46:28 by ale              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,8 @@ int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-// void	*ft_memchr(const void *s, int c, size_t n);
-// int		ft_memcmp(const void *s1, const void *s2, size_t n);
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
